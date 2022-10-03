5d5dc28ec3069743c36fe938c8276641c5acd840
BlashchukNG <blashchukng@outlook.com>
2022-10-03 19:38:10 +0300

    create input service

--- Assets/Code/Infrastructure/Game.cs
+++ Assets/Code/Infrastructure/Game.cs
@@ -1,7 +1,19 @@
-﻿namespace Code.Infrastructure
+﻿using Code.Services.Input;
+
+namespace Code.Infrastructure
 {
     public sealed class Game
     {
-        
+        public static IInputService inputService;
+
+        public Game()
+        {
+            RegisterInputService();
+        }
+
+        private void RegisterInputService()
+        {
+            inputService = new InputService();
+        }
     }
 }
---
+++ Assets/Code/Services/Input/IInputService.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Code.Services.Input
+{
+    public interface IInputService
+    {
+        Vector2 Axis { get; }
+
+        bool IsAttackButtonUp();
+    }
+}
