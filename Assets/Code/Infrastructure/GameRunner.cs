bbe9cdf37582717bc62c8bc8fe3f19cafa9ad711
Grigorich <blashchukng@outlook.com>
2022-10-11 19:01:52 +0300

    game runner

---
+++ Assets/Code/Infrastructure/GameRunner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Code.Infrastructure
+{
+    public sealed class GameRunner :
+        MonoBehaviour
+    {
+        [SerializeField] private GameBootstrapper _bootstrapper;
+
+        private void Awake()
+        {
+            var bootstrapper = FindObjectOfType<GameBootstrapper>();
+
+            if (bootstrapper == null) Instantiate(_bootstrapper);
+        }
+    }
+}
